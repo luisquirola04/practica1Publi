b4d2518ab9817e0a2f0ed9d91f0c403fff892815
luisquirola04 <luisangelangel38@gmail.com>
2024-06-20 17:35:10 -0500

    subiendo practica terminada

--- models/status.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-class Status(enum.Enum):
-    Expired                 =           'Expired'
-    Good                    =           'Good'
-    AlmostExpired                    =  'AlmostExpired'
--- routes/schemas/schemas_batch.py
+++
@@ -1,9 +0,0 @@
-schema_save_batch = {
-    "type": "object",
-    "properties": {
-        "maker": {"type": "string"},
-        "price_batch": {"type": "number"},
-        "arrive_date":{"type": "string", "format": "date-time"}
-    },
-    "required": ["maker", "price_batch", "arrive_date"]
-}
